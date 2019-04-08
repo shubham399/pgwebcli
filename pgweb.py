8f5c502ded2590e8f378cec2509c1bef491fea15
Shubham <shubham.399@gmail.com>
2019-04-08 19:27:07 +0530

    Added to \d "table" to list table structure and \d to list the tables.

--- pgweb.py
+++ pgweb.py
@@ -7,6 +7,7 @@ from tabulate import tabulate
 PG_WEB_URL= os.getenv('PG_WEB_URL', 'https://localhost')
 COOKIE_KEY = os.getenv('COOKIE_KEY','COOKIE_KEY')
 COOKIE_VALUE = os.getenv('COOKIE_VALUE','COOKIE_VALUE')
+schema = ""
 # CODE BEGIN
 isExpanded = False
 cookies = {
@@ -34,25 +35,75 @@ def getShell():
   while True:
     try:
       cmd = raw_input("pgweb# ").strip()
-      if cmd == "" : continue
-      if (cmd == "q" or cmd == "quit") : break
-      if (cmd == "c" or cmd == "clear" ):
-        _ = system('clear')
+      rescode = commandHandler(cmd)
+      if rescode == 0:
         continue
-      if (cmd == "x" or cmd == "extend") :
-        global isExpanded
-        isExpanded = not isExpanded
-        if isExpanded :
-          print "Expanded display is on."
-        else :
-          print "Expanded display is off."
-        continue
-      if cmd == "h" or cmd == "help" :
-        print "press c to clear screen, q to quit or pass the sql query."
-        continue
-      query(cmd)
+      elif rescode == 1:
+        break
+      elif rescode == 2:
+        query(cmd)
     except:
       break
+
+def commandHandler(cmd):
+  command = cmd.split(" ")
+  if command[0] == "" : return 0
+  if (command[0] == "\\q" or command[0] == "quit") : return 1
+  if (command[0] == "\\c" or command[0] == "clear" ):
+    _ = system('clear')
+    return 0
+  if (command[0] == "\\x" or command[0] == "extend") :
+    global isExpanded
+    isExpanded = not isExpanded
+    if isExpanded :
+      print "Expanded display is on."
+    else :
+      print "Expanded display is off."
+    return 0
+  if command[0] == "\\h" or command[0] == "help" :
+    print "press \\c to clear screen, \\q to quit, \\d to list tables \\d tablename to structure or pass the sql query."
+    return 0
+  if command[0] == "\\d":
+    if len(command) != 2:
+      printObject()
+      return 0
+    else :
+      expandTable(command[1])
+      return 0
+  if len(command) == 3 :
+    if command[0].upper() == "SET" and command[1].upper() == "SCHEMA" :
+      global schema
+      schema = command[2].replace("\'","").replace("\"","").replace(";","")
+
+  return 2
+
+def expandTable(input):
+  input = input.replace("\"","")
+  input = input if schema in input else schema + "."+input
+  response = requests.get(PG_WEB_URL+'/api/tables/'+str(input), headers=headers, cookies=cookies)
+  data = response.json()
+  if 'error' in data.keys():
+    print data["error"]
+  else:
+    print tabulate(data["rows"], headers=data["columns"], tablefmt='psql')
+    printIndexs(input)
+
+def printIndexs(input):
+  response = requests.get(PG_WEB_URL+'/api/tables/'+str(input)+"/indexes", headers=headers, cookies=cookies)
+  data = response.json()
+  if 'error' in data.keys():
+    print data["error"]
+  else:
+    print tabulate(data["rows"], headers=data["columns"], tablefmt='psql')
+
+def printObject():
+  response = requests.get(PG_WEB_URL+'/api/objects', headers=headers, cookies=cookies)
+  data = response.json()
+  if 'error' in data.keys():
+    print data["error"]
+  else:
+    key = data.keys()[0]
+    print tabulate(data[key], tablefmt='psql')
 
 def query(input):
   data = { 'query' : input}
