0ba37a4a18085835be15d920aee5bfe6ea0d87fa
Shubham <shubham.399@gmail.com>
2019-04-08 18:19:05 +0530

    Fixed the Enter bug and code cleanup.

--- pgweb.py
+++ pgweb.py
@@ -1,28 +1,59 @@
-# EDIT THESE BEFORE USE
-import os
+import requests
+import json, sys,readline,signal ,select,os
+from tabulate import tabulate
+
+# SET ENV FOR THESE
 PG_WEB_URL= os.getenv('PG_WEB_URL', 'https://localhost')
 COOKIE_KEY = os.getenv('COOKIE_KEY','COOKIE_KEY')
 COOKIE_VALUE = os.getenv('COOKIE_VALUE','COOKIE_VALUE')
 # CODE BEGIN
-from os import system
-import requests
-import json, sys,readline,signal
-from tabulate import tabulate
-def signal_handler(sig, frame):
-        sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-
 isExpanded = False
-
 cookies = {
     COOKIE_KEY: COOKIE_VALUE,
 }
-
 headers = {
     'cache-control': 'no-cache',
 }
 
-def callAPI (input):
+def main ():
+  handlePipe()
+  getShell()
+
+def handlePipe():
+  try:
+    line, w, x = select.select([sys.stdin], [], [], 0)
+    if  len(line) > 0:
+      query(line[0])
+      sys.exit(0)
+  except:
+    sys.exit(1)
+
+def getShell():
+  print "USING HOST: " + str(PG_WEB_URL)
+  while True:
+    try:
+      cmd = raw_input("pgweb# ").strip()
+      if cmd == "" : continue
+      if (cmd == "q" or cmd == "quit") : break
+      if (cmd == "c" or cmd == "clear" ):
+        _ = system('clear')
+        continue
+      if (cmd == "x" or cmd == "extend") :
+        global isExpanded
+        isExpanded = not isExpanded
+        if isExpanded :
+          print "Expanded display is on."
+        else :
+          print "Expanded display is off."
+        continue
+      if cmd == "h" or cmd == "help" :
+        print "press c to clear screen, q to quit or pass the sql query."
+        continue
+      query(cmd)
+    except:
+      break
+
+def query(input):
   data = { 'query' : input}
   response = requests.post(PG_WEB_URL+'/api/query', headers=headers, cookies=cookies, data=data)
   data = response.json()
@@ -45,34 +76,11 @@ def makeExtendedJSON (data):
       extendedData.append(value)
     print tabulate(extendedData, tablefmt='psql')
 
-try:
-  line = raw_input()
-  if line != "":
-    callAPI (line)
-    sys.exit(0)
-except:
-  sys.exit(1)
 
-print "USING HOST: " + str(PG_WEB_URL)
-while True:
-    try:
-      cmd = raw_input("pgweb# ").strip()
-      if cmd == "" : continue
-      if (cmd == "q" or cmd == "quit") : break
-      if (cmd == "c" or cmd == "clear" ):
-        _ = system('clear')
-        continue
-      if (cmd == "x" or cmd == "extend") :
-          isExpanded = not isExpanded
-          if isExpanded :
-            print "Expanded display is on."
-          else :
-            print "Expanded display is off."
-          continue
-      if cmd == "h" or cmd == "help" :
-        print "press c to clear screen, q to quit or pass the sql query."
-        continue
-      callAPI (cmd)
-    except:
-      break
-# CODE END
+def signal_handler(sig, frame):
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+
+if __name__ == "__main__":
+    main()
